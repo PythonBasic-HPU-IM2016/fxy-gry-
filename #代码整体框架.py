9ebf6cf587c352b1c3935d34e97100018434e449
FXXYYY <412135554@qq.com>
2019-01-10 15:22:06 +0800

    Create #代码整体框架.py

---
+++ #代码整体框架.py
@@ -0,0 +1,79 @@
+#代码整体框架
+
+#引用库
+
+#创建窗口
+
+
+def GameStart():
+
+
+    #游戏背景对象
+    
+    Background = pygame.image.load()
+    
+    #挡板背景对象
+
+    Baddle = pygame.image.load()
+
+    #球对象    
+
+    Ball = pygame.image.load()
+
+    #挡板位置信息
+
+    BaffleX
+    BaffleY
+
+    #球位置信息
+
+    BallX
+    ballY
+    BallSpeed
+
+    #帧率控制Clock对象
+
+    #显示时间Clock对象
+
+    #设置时间字体
+
+    #游戏结果
+
+
+    while True:
+        #接受信息处理
+
+        #绘制背景
+
+        #显示时间
+
+        #绘制球
+
+        #判断球边界条件
+
+        #定位板移动后坐标
+
+        #判断挡板边界条件
+
+        #刷新显示
+        
+
+
+
+def GameResult():
+
+
+    #游戏结果背景Surface对象
+
+    #游戏结果引导
+
+    # 游戏结果Font对象
+
+    # 重新开始按钮
+
+    # 重新开始Hover按钮
+
+    # 游戏结果
+
+
+if __name__ == "__main__":
